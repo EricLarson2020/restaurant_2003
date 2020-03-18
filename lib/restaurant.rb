120b8113988df9f9fe5a09157513a76a45d35a6e
Eric Larson <ericlarsonbroom@gmail.com>
2020-03-18 13:21:25 -0600

    added material

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,1 +1,17 @@
+class Restaurant
+  attr_reader :opening_time, :name, :dishes
 
+def initialize (opening_time, name)
+  @opening_time = opening_time
+  @name = name
+  @dishes = []
+end
+
+def closing_time(time)
+  @opening_time.tr(':00', "").to_i + time
+end
+
+def add_dish(dish)
+  @dishes << dish
+end
+end
